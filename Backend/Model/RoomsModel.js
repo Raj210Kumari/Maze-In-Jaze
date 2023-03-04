ee1ebef13f6567595f41f2b72023b350fb379980
swarup-1 <swarupkadoli9@gmail.com>
2023-03-04 19:53:42 +0530

    Backend Changes

--- Backend/Model/RoomsModel.js
+++ Backend/Model/RoomsModel.js
@@ -4,8 +4,10 @@ const roomSchema = mongoose.Schema({
     roomID:String,
     username1: String,
     userID1: String,
+    userScore1:Number,
     username2: String,
     userID2: String,
+    userScore2:Number,
     winner:String,
 })
 const RoomModel = mongoose.model("room",roomSchema)
