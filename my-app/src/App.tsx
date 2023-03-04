85599c59167caf2bd67ca0198854bcd00d805dcc
swarup-1 <swarupkadoli9@gmail.com>
2023-03-04 23:11:33 +0530

    Maze-in-Jaze Completed

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,14 @@
+import { Box } from '@chakra-ui/layout';
 import React from 'react';
 import MainRoute from './AllRoutes/MainRoute';
 import './App.css';
 import {Navbar} from './Page/Navbar';
 function App() {
   return (
-    <div className="App">
+    <Box h="100vh" className="App">
       <Navbar/>
       <MainRoute/>
-    </div>
+    </Box>
   );
 }
 
